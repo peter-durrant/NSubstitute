5264869ea2b1234c5f4cc83738aca91f68fb9d58
Peter Durrant <peterjdurrant@gmail.com>
2016-05-27 15:02:20 +0100

    NSubstitute extension for StartsInOrder

---
+++ Received.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSubstitute.Core;
+using NSubstitute.Core.SequenceChecking;
+
+namespace NSubstituteExtension
+{
+   public static class Received
+   {
+      public static void StartsInOrder(Action calls)
+      {
+         var queryResults = SubstitutionContext.Current.RunQuery(calls);
+         var results = new Results(queryResults);
+         new SequenceInOrderAssertion().Assert(results);
+      }
+
+      private class Results : IQueryResults
+      {
+         private readonly IQueryResults _results;
+
+         public Results(IQueryResults results)
+         {
+            _results = results;
+         }
+
+         public IEnumerable<ICall> MatchingCallsInOrder()
+         {
+            return _results.MatchingCallsInOrder().Take(QuerySpecification().Count());
+         }
+
+         public IEnumerable<CallSpecAndTarget> QuerySpecification()
+         {
+            return _results.QuerySpecification();
+         }
+      }
+   }
+}
---
+++ ReceivedTest.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Extensions.Test
+{
+   [TestClass]
+   public class ReceivedTest
+   {
+      public interface IExampleModel
+      {
+         void Function1();
+         void Function2();
+         void Function3();
+      }
+
+      private void CallMethods(IExampleModel model)
+      {
+         model.Function1();
+         model.Function2();
+         model.Function3();
+      }
+
+      /// <summary>
+      /// Count of received calls on model - order is unimportant.
+      /// </summary>
+      [TestMethod]
+      public void Received_NSubstituteReceivedCalls_ReceivesExactly()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         model.Received(1).Function1();
+         model.Received(1).Function2();
+         model.Received(1).Function3();
+      }
+
+      /// <summary>
+      /// model receives exactly the calls specified in order.
+      /// </summary>
+      [TestMethod]
+      public void Received_NSubstituteReceivesInOrder_ReceivesInOrder()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         Received.InOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+            model.Function3();
+         });
+      }
+
+      /// <summary>
+      /// model receives the calls specified in order - other calls were made to the mocked object.
+      /// </summary>
+      [TestMethod]
+      public void Received_NSubstituteReceivesInOrder_ReceivesInOrderPartial()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         Received.InOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+         });
+      }
+
+      /// <summary>
+      /// model receives the calls specified in order - other calls were made to the mocked object.
+      /// </summary>
+      [TestMethod]
+      public void Received_NSubstituteReceivesStartsInOrder_ReceivesInOrderPartial()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         NSubstituteExtension.Received.StartsInOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+         });
+      }
+
+      /// <summary>
+      /// This test demonstrates that only the exact sequence of calls on model are permitted.
+      /// In this case, the test of InOrder fails because more calls were detected than specified.
+      /// </summary>
+      [TestMethod]
+      [ExpectedException(typeof(NSubstitute.Exceptions.CallSequenceNotFoundException))]
+      public void Received_NSubstituteReceivesInOrder_ThrowsExceptionTooManyItemsReceieved()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+         CallMethods(model);
+
+         // assert
+         Received.InOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+            model.Function3();
+         });
+      }
+
+      /// <summary>
+      /// This test demonstrates that any sequence of calls are permitted on model so long as they start in the correct order.
+      /// </summary>
+      [TestMethod]
+      public void Received_NSubstituteExtensionStartsInOrder_ReceivesInOrder()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+         CallMethods(model);
+
+         // assert
+         NSubstituteExtension.Received.StartsInOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+            model.Function3();
+         });
+      }
+
+      #region Tests that do not make enough calls
+
+      /// <summary>
+      /// This test demonstrates that more calls were expected on model than received - InOrder.
+      /// In this case, the test of InOrder fails because more calls were specified than detected.
+      /// </summary>
+      [TestMethod]
+      [ExpectedException(typeof(NSubstitute.Exceptions.CallSequenceNotFoundException))]
+      public void Received_NSubstituteTooManyMethodCallsExpected_ThrowsException()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         Received.InOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+            model.Function3();
+            // additional function call
+            model.Function1();
+         });
+      }
+
+      /// <summary>
+      /// This test demonstrates that more calls were expected on model than received - StartsInOrder.
+      /// In this case, the test of StartsInOrder fails because more calls were specified than detected.
+      /// </summary>
+      [TestMethod]
+      [ExpectedException(typeof(NSubstitute.Exceptions.CallSequenceNotFoundException))]
+      public void Received_NSubstituteExtensionStartsInOrderTooManyMethodCallsExpected_ThrowsException()
+      {
+         // arrange
+         var model = Substitute.For<IExampleModel>();
+
+         // act
+         CallMethods(model);
+
+         // assert
+         NSubstituteExtension.Received.StartsInOrder(() =>
+         {
+            model.Function1();
+            model.Function2();
+            model.Function3();
+            // additional function call
+            model.Function1();
+         });
+      }
+
+      #endregion
+   }
+}
